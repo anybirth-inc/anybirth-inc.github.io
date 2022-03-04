e7f6fc85f73929d57110eabfc5346113629cdb36
naokiupa <naokiupa@gmail.com>
2022-03-04 17:12:48 +0900

    set all func

--- script.js
+++ script.js
@@ -3,10 +3,276 @@ const app = createApp({
     data() {
         return {
             popUp: false,
+            shipping_day_turn: 2,
             item: {
                 name: "わくわくロス野菜10kgパック",
                 price: 2480,
             },
+            items: [{
+                    name: "5kg パック / 月１配送 / 第２木曜出荷",
+                    stripe_name: "わくわくロス野菜5kgパック",
+                    weight: "5kg",
+                    frequency: "monthly",
+                    shipping_day: "thursday",
+                    shipping_day_turn: 2,
+                    price: 1480,
+                    shipping_fee: 800,
+                    url: "https://buy.stripe.com/3cseWM5KR2x26GIeUU",
+                },
+                {
+                    name: "5kg パック / 月１配送 / 第２金曜出荷",
+                    stripe_name: "わくわくロス野菜5kgパック",
+                    weight: "5kg",
+                    frequency: "monthly",
+                    shipping_day: "friday",
+                    shipping_day_turn: 2,
+                    price: 1480,
+                    shipping_fee: 800,
+                    url: "https://buy.stripe.com/5kA15W2yF7Rmfde3cd",
+                },
+                {
+                    name: "5kg パック / 月１配送 / 第4木曜出荷",
+                    stripe_name: "わくわくロス野菜5kgパック",
+                    weight: "5kg",
+                    frequency: "monthly",
+                    shipping_day: "thursday",
+                    shipping_day_turn: 4,
+                    price: 1480,
+                    shipping_fee: 800,
+                    url: "https://buy.stripe.com/bIY7uk1uB2x2fdeaEG",
+                },
+                {
+                    name: "5kg パック / 月１配送 / 第4金曜出荷",
+                    stripe_name: "わくわくロス野菜5kgパック",
+                    weight: "5kg",
+                    frequency: "monthly",
+                    shipping_day: "friday",
+                    shipping_day_turn: 4,
+                    price: 1480,
+                    shipping_fee: 800,
+                    url: "https://buy.stripe.com/fZedSI2yFgnS4yA7sv",
+                },
+                {
+                    name: "5kg パック / 2週間1配送 / 第２木曜出荷",
+                    stripe_name: "わくわくロス野菜5kgパック",
+                    weight: "5kg",
+                    frequency: "each 2 weeks",
+                    shipping_day: "thursday",
+                    shipping_day_turn: 2,
+                    price: 2960,
+                    shipping_fee: 1600,
+                    url: "https://buy.stripe.com/bIYdSIgpv5JeaWY148",
+                },
+                {
+                    name: "5kg パック / 2週間1配送 / 第２金曜出荷",
+                    stripe_name: "わくわくロス野菜5kgパック",
+                    weight: "5kg",
+                    frequency: "each 2 weeks",
+                    shipping_day: "friday",
+                    shipping_day_turn: 2,
+                    price: 2960,
+                    shipping_fee: 1600,
+                    url: "https://buy.stripe.com/dR63e43CJfjO4yA9AF",
+                },
+                {
+                    name: "5kg パック / 2週間1配送 / 第4木曜出荷",
+                    stripe_name: "わくわくロス野菜5kgパック",
+                    weight: "5kg",
+                    frequency: "each 2 weeks",
+                    shipping_day: "thursday",
+                    shipping_day_turn: 4,
+                    price: 2960,
+                    shipping_fee: 1600,
+                    url: "https://buy.stripe.com/5kAg0Q5KRb3y4yA28e",
+                },
+                {
+                    name: "5kg パック / 2週間1配送 / 第4金曜出荷",
+                    stripe_name: "わくわくロス野菜5kgパック",
+                    weight: "5kg",
+                    frequency: "each 2 weeks",
+                    shipping_day: "friday",
+                    shipping_day_turn: 4,
+                    price: 2960,
+                    shipping_fee: 1600,
+                    url: "https://buy.stripe.com/3csg0Q1uB2x26GIcMT",
+                },
+                {
+                    name: "10kg パック / 月１配送 / 第２木曜出荷",
+                    stripe_name: "わくわくロス野菜10kgパック",
+                    weight: "10kg",
+                    frequency: "monthly",
+                    shipping_day: "thursday",
+                    shipping_day_turn: 2,
+                    price: 2480,
+                    shipping_fee: 900,
+                    url: "https://buy.stripe.com/7sI2a06OV3B6ghiaEM",
+                },
+                {
+                    name: "10kg パック / 月１配送 / 第２金曜出荷",
+                    stripe_name: "わくわくロス野菜10kgパック",
+                    weight: "10kg",
+                    frequency: "monthly",
+                    shipping_day: "friday",
+                    shipping_day_turn: 2,
+                    price: 2480,
+                    shipping_fee: 900,
+                    url: "https://buy.stripe.com/14k3e40qx1sYghi7sB",
+                },
+                {
+                    name: "10kg パック / 月１配送 / 第4木曜出荷",
+                    stripe_name: "わくわくロス野菜10kgパック",
+                    weight: "10kg",
+                    frequency: "monthly",
+                    shipping_day: "thursday",
+                    shipping_day_turn: 4,
+                    price: 2480,
+                    shipping_fee: 900,
+                    url: "https://buy.stripe.com/00g9Csc9f4Fac127sC",
+                },
+                {
+                    name: "10kg パック / 月１配送 / 第4金曜出荷",
+                    stripe_name: "わくわくロス野菜10kgパック",
+                    weight: "10kg",
+                    frequency: "monthly",
+                    shipping_day: "friday",
+                    shipping_day_turn: 4,
+                    price: 2480,
+                    shipping_fee: 900,
+                    url: "https://buy.stripe.com/14k7uk6OV0oU9SU14f",
+                },
+                {
+                    name: "10kg パック / 2週間1配送 / 第２木曜出荷",
+                    stripe_name: "わくわくロス野菜10kgパック",
+                    weight: "10kg",
+                    frequency: "each 2 weeks",
+                    shipping_day: "thursday",
+                    shipping_day_turn: 2,
+                    price: 4960,
+                    shipping_fee: 1800,
+                    url: "https://buy.stripe.com/bIY8yo3CJfjO1mofZa",
+                },
+                {
+                    name: "10kg パック / 2週間1配送 / 第２金曜出荷",
+                    stripe_name: "わくわくロス野菜10kgパック",
+                    weight: "10kg",
+                    frequency: "each 2 weeks",
+                    shipping_day: "friday",
+                    shipping_day_turn: 2,
+                    price: 4960,
+                    shipping_fee: 1800,
+                    url: "https://buy.stripe.com/28o5mcgpvefK2qs8wJ",
+                },
+                {
+                    name: "10kg パック / 2週間1配送 / 第4木曜出荷",
+                    stripe_name: "わくわくロス野菜10kgパック",
+                    weight: "10kg",
+                    frequency: "each 2 weeks",
+                    shipping_day: "thursday",
+                    shipping_day_turn: 4,
+                    price: 4960,
+                    shipping_fee: 1800,
+                    url: "https://buy.stripe.com/3cs15W4GNb3y9SUfZc",
+                },
+                {
+                    name: "10kg パック / 2週間1配送 / 第4金曜出荷",
+                    stripe_name: "わくわくロス野菜10kgパック",
+                    weight: "10kg",
+                    frequency: "each 2 weeks",
+                    shipping_day: "friday",
+                    shipping_day_turn: 4,
+                    price: 4960,
+                    shipping_fee: 1800,
+                    url: "https://buy.stripe.com/3cs9Cs6OVfjO5CE9AP",
+                },
+                {
+                    name: "20kg パック / 月１配送 / 第２木曜出荷",
+                    stripe_name: "わくわくロス野菜20kgパック",
+                    weight: "20kg",
+                    frequency: "monthly",
+                    shipping_day: "thursday",
+                    shipping_day_turn: 2,
+                    price: 3980,
+                    shipping_fee: 1400,
+                    url: "https://buy.stripe.com/7sI3e45KRc7C4yA28o",
+                },
+                {
+                    name: "20kg パック / 月１配送 / 第２金曜出荷",
+                    stripe_name: "わくわくロス野菜20kgパック",
+                    weight: "20kg",
+                    frequency: "monthly",
+                    shipping_day: "friday",
+                    shipping_day_turn: 2,
+                    price: 3980,
+                    shipping_fee: 1400,
+                    url: "https://buy.stripe.com/eVaaGwflr5Je6GIfZf",
+                },
+                {
+                    name: "20kg パック / 月１配送 / 第4木曜出荷",
+                    stripe_name: "わくわくロス野菜20kgパック",
+                    weight: "20kg",
+                    frequency: "monthly",
+                    shipping_day: "thursday",
+                    shipping_day_turn: 4,
+                    price: 3980,
+                    shipping_fee: 1400,
+                    url: "https://buy.stripe.com/8wM01S1uBb3yc128wO",
+                },
+                {
+                    name: "20kg パック / 月１配送 / 第4金曜出荷",
+                    stripe_name: "わくわくロス野菜20kgパック",
+                    weight: "20kg",
+                    frequency: "monthly",
+                    shipping_day: "friday",
+                    shipping_day_turn: 4,
+                    price: 3980,
+                    shipping_fee: 1400,
+                    url: "https://buy.stripe.com/28odSIb5b0oU0ik5kD",
+                },
+                {
+                    name: "20kg パック / 2週間1配送 / 第２木曜出荷",
+                    stripe_name: "わくわくロス野菜20kgパック",
+                    weight: "20kg",
+                    frequency: "each 2 weeks",
+                    shipping_day: "thursday",
+                    shipping_day_turn: 2,
+                    price: 7960,
+                    shipping_fee: 2800,
+                    url: "https://buy.stripe.com/8wM4i8flr5Je0ikaEY",
+                },
+                {
+                    name: "20kg パック / 2週間1配送 / 第２金曜出荷",
+                    stripe_name: "わくわくロス野菜20kgパック",
+                    weight: "20kg",
+                    frequency: "each 2 weeks",
+                    shipping_day: "friday",
+                    shipping_day_turn: 2,
+                    price: 7960,
+                    shipping_fee: 2800,
+                    url: "https://buy.stripe.com/14k15Wc9f1sYc129AV",
+                },
+                {
+                    name: "20kg パック / 2週間1配送 / 第4木曜出荷",
+                    stripe_name: "わくわくロス野菜20kgパック",
+                    weight: "20kg",
+                    frequency: "each 2 weeks",
+                    shipping_day: "thursday",
+                    shipping_day_turn: 4,
+                    price: 7960,
+                    shipping_fee: 2800,
+                    url: "https://buy.stripe.com/28o8yoflrfjOfdecN8",
+                },
+                {
+                    name: "20kg パック / 2週間1配送 / 第4金曜出荷",
+                    stripe_name: "わくわくロス野菜20kgパック",
+                    weight: "20kg",
+                    frequency: "each 2 weeks",
+                    shipping_day: "friday",
+                    shipping_day_turn: 4,
+                    price: 7960,
+                    shipping_fee: 2800,
+                    url: "https://buy.stripe.com/8wM4i84GNefKfde3cz",
+                },
+            ],
             shipping_fee: 900,
             kind: 1,
             kind_list: [{
@@ -19,27 +285,31 @@ const app = createApp({
                 },
                 {
                     id: 3,
-                    name: "15kg",
+                    name: "20kg",
                 }
             ],
             shipping_frequency: 1,
             shipping_frequency_list: [{
                     id: 1,
                     name: "1ヶ月ごと",
+                    en_name: "monthly",
                 },
                 {
                     id: 2,
                     name: "2週間ごと",
+                    en_name: "each 2 weeks",
                 }
             ],
             shipping_day: 1,
             shipping_day_list: [{
                     id: 1,
                     name: "木曜日",
+                    en_name: "thursday"
                 },
                 {
                     id: 2,
                     name: "金曜日",
+                    en_name: "friday",
                 }
             ]
         }
@@ -57,10 +327,47 @@ const app = createApp({
                 this.kind = kind
             }
             // this.$set(this, "popUp", !this.popUp);
+            this.resetCart();
             this.popUp = !this.popUp
         },
+        resetCart() {
+            const item = this.getItem();
+            this.item.name = item.stripe_name;
+            this.item.price = item.price;
+            this.shipping_fee = item.shipping_fee / 1.1;
+        },
+        getItem() {
+            const kind = this.kind_list.find(
+                (u) => u.id === this.kind
+            );
+            const shipping_frequency = this.shipping_frequency_list.find(
+                (u) => u.id === this.shipping_frequency
+            );
+            const shipping_day = this.shipping_day_list.find(
+                (u) => u.id === this.shipping_day
+            );
+            const result = this.items.filter(
+                (u) =>
+                u.weight === kind.name &&
+                u.frequency === shipping_frequency.en_name &&
+                u.shipping_day === shipping_day.en_name &&
+                u.shipping_day_turn === this.shipping_day_turn
+            );
+            return result[0];
+        },
         goToCheckout() {
-
+            const url = this.getItem();
+            // if (result.length == 1) {
+            // if (result[0] != null) {
+            window.location.href = url["url"];
+            // } else {
+            //     console.log("error: 2");
+            //     window.location.href = result[0]["url"];
+            // }
+            // } else {
+            //     console.log("error: 1");
+            //     window.location.href = result[0]["url"];
+            // }
         },
     },
     computed: {
